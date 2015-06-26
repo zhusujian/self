03ecaa4aee96929667a8bc02020b2a307de078cf
admin <NT60388@cathaybk.com.tw>
2015-06-26 12:54:06 +0800

    Add Demo.java File

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello {
 	public static void main(String args[]){
-		System.out.println("Hello Yootk!");
-		System.out.println("Hello www.yootk.com");
+		System.out.println("www.mldn.cn");
 	}
 }
