d66df8c8c06cb11907b9f4bb3f809ce0a27fc195
admin <NT60388@cathaybk.com.tw>
2015-06-29 17:17:10 +0800

    Change Two Files Print Content

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
 	public static void main(String args[]){
-		System.out.println("HELLO yootk");
+		System.out.println("Hello World");
 	}
 }
--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
 	public static void main(String args[]){
-		System.out.println("yootk");
+		System.out.println("www.yootk.com");
 	}
 }
